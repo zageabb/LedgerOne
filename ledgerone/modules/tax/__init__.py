564763d6acbe29e0f7c39dd42e4c4ba17eb463f0
zageabb <zageabb@users.noreply.github.com>
2026-09-14 22:33:22 +0100

    Expose tax default seeding hook

--- ledgerone/modules/tax/__init__.py
+++ ledgerone/modules/tax/__init__.py
@@ -1,8 +1,13 @@
 from ledgerone.modules.tax import models  # noqa: F401
 from ledgerone.modules.tax.api import api_bp
 from ledgerone.modules.tax.routes import bp
+from ledgerone.modules.tax.services import TaxService
 
 
 def register(app):
     app.register_blueprint(bp)
     app.register_blueprint(api_bp)
+
+
+def seed_defaults(organisation_id: str):
+    TaxService.seed_defaults(organisation_id)
