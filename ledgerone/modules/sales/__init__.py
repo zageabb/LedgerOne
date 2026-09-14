4155a4a0bfce681e9e5ba76bbb8934b9a31232be
zageabb <zageabb@users.noreply.github.com>
2026-09-14 22:43:37 +0100

    Register sales credit note routes and API

--- ledgerone/modules/sales/__init__.py
+++ ledgerone/modules/sales/__init__.py
@@ -1,8 +1,12 @@
 from ledgerone.modules.sales import credit_models, models  # noqa: F401
 from ledgerone.modules.sales.api import api_bp
+from ledgerone.modules.sales.credit_api import api_bp as credit_api_bp
+from ledgerone.modules.sales.credit_routes import bp as credit_bp
 from ledgerone.modules.sales.routes import bp
 
 
 def register(app):
     app.register_blueprint(bp)
     app.register_blueprint(api_bp)
+    app.register_blueprint(credit_bp)
+    app.register_blueprint(credit_api_bp)
