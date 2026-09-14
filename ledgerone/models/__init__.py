0ddc2f23d29655fcd7aa549c20e73c593167e827
zageabb <zageabb@users.noreply.github.com>
2026-09-14 17:14:48 +0100

    Export audit event model

--- ledgerone/models/__init__.py
+++ ledgerone/models/__init__.py
@@ -1,7 +1,9 @@
+from ledgerone.models.audit import AuditEvent
 from ledgerone.models.core import ApiKey, Membership, ModuleState, Organisation, Setting, User
 from ledgerone.models.ledger import Account, Journal, JournalLine
 
 __all__ = [
+    "AuditEvent",
     "ApiKey",
     "Membership",
     "ModuleState",
