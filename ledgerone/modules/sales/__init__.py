47d472c19ba0a255bab9acd87ddf2603ac76afaa
zageabb <zageabb@users.noreply.github.com>
2026-09-14 22:54:06 +0100

    Register sales quote models routes and API

--- ledgerone/modules/sales/__init__.py
+++ ledgerone/modules/sales/__init__.py
@@ -1,7 +1,9 @@
-from ledgerone.modules.sales import credit_models, models  # noqa: F401
+from ledgerone.modules.sales import credit_models, models, quote_models  # noqa: F401
 from ledgerone.modules.sales.api import api_bp
 from ledgerone.modules.sales.credit_api import api_bp as credit_api_bp
 from ledgerone.modules.sales.credit_routes import bp as credit_bp
+from ledgerone.modules.sales.quote_api import api_bp as quote_api_bp
+from ledgerone.modules.sales.quote_routes import bp as quote_bp
 from ledgerone.modules.sales.routes import bp
 
 
@@ -10,3 +12,5 @@ def register(app):
     app.register_blueprint(api_bp)
     app.register_blueprint(credit_bp)
     app.register_blueprint(credit_api_bp)
+    app.register_blueprint(quote_bp)
+    app.register_blueprint(quote_api_bp)
