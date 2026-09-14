47bf8df4ded34cd72682390b7e0e81091bd6d2c6
zageabb <zageabb@users.noreply.github.com>
2026-09-14 17:17:10 +0100

    Add CSRF extension

--- ledgerone/extensions.py
+++ ledgerone/extensions.py
@@ -1,11 +1,13 @@
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 migrate = Migrate()
+csrf = CSRFProtect()
 
 login_manager.login_view = "auth.login"
 login_manager.login_message_category = "warning"
