349ddbfc84ebbb4f579b31b1f8ba7555396c7157
zageabb <zageabb@users.noreply.github.com>
2026-09-14 17:14:24 +0100

    Register settings module

---
+++ ledgerone/modules/settings/__init__.py
@@ -0,0 +1,7 @@
+from ledgerone.modules.settings.api import api_bp
+from ledgerone.modules.settings.routes import bp
+
+
+def register(app):
+    app.register_blueprint(bp)
+    app.register_blueprint(api_bp)
