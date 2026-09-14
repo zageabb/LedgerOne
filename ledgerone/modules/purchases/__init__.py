c531e143fa3dacbc1561eb787d64a4f7e9c7eabc
zageabb <zageabb@users.noreply.github.com>
2026-09-14 23:14:02 +0100

    Register purchase order models routes and API

--- ledgerone/modules/purchases/__init__.py
+++ ledgerone/modules/purchases/__init__.py
@@ -1,7 +1,9 @@
-from ledgerone.modules.purchases import credit_models, models  # noqa: F401
+from ledgerone.modules.purchases import credit_models, models, order_models  # noqa: F401
 from ledgerone.modules.purchases.api import api_bp
 from ledgerone.modules.purchases.credit_api import api_bp as credit_api_bp
 from ledgerone.modules.purchases.credit_routes import bp as credit_bp
+from ledgerone.modules.purchases.order_api import api_bp as order_api_bp
+from ledgerone.modules.purchases.order_routes import bp as order_bp
 from ledgerone.modules.purchases.routes import bp
 
 
@@ -10,3 +12,5 @@ def register(app):
     app.register_blueprint(api_bp)
     app.register_blueprint(credit_bp)
     app.register_blueprint(credit_api_bp)
+    app.register_blueprint(order_bp)
+    app.register_blueprint(order_api_bp)
