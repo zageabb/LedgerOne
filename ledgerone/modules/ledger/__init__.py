0dca3de13e922c70bd4dd0374d15ade5f419edc6
zageabb <zageabb@users.noreply.github.com>
2026-09-15 16:13:01 +0100

    Register accounting period policy API

--- ledgerone/modules/ledger/__init__.py
+++ ledgerone/modules/ledger/__init__.py
@@ -1,5 +1,6 @@
 from ledgerone.modules.ledger.api import api_bp
 from ledgerone.modules.ledger.control_api import control_api_bp
+from ledgerone.modules.ledger.period_api import period_api_bp
 from ledgerone.modules.ledger.routes import bp
 
 
@@ -7,3 +8,4 @@ def register(app):
     app.register_blueprint(bp)
     app.register_blueprint(api_bp)
     app.register_blueprint(control_api_bp)
+    app.register_blueprint(period_api_bp)
