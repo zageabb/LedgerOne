65af4b99d6ebdf1517945462f5c67b1a550e2e97
zageabb <zageabb@users.noreply.github.com>
2026-09-16 07:54:48 +0100

    Register numbering management API

--- ledgerone/modules/settings/__init__.py
+++ ledgerone/modules/settings/__init__.py
@@ -1,7 +1,9 @@
 from ledgerone.modules.settings.api import api_bp
+from ledgerone.modules.settings.numbering_api import numbering_api_bp
 from ledgerone.modules.settings.routes import bp
 
 
 def register(app):
     app.register_blueprint(bp)
     app.register_blueprint(api_bp)
+    app.register_blueprint(numbering_api_bp)
